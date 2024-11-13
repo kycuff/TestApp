6aa77ecceaec14be0af97e6c6f9b35d7a18b9dd5
kycuff <kylecuthbert32@gmail.com>
2024-11-13 10:35:46 +0000

    Add project files.

---
+++ TestApp/UserDataViewModel.cs
@@ -0,0 +1,20 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace TestApp;
+public partial class UserDataViewModel : ObservableObject
+{
+    public UsersDataModel UsersData { get; set; }
+    public UserDataViewModel()
+    {
+        UsersData = new UsersDataModel
+        {
+            InstalmentPlans = new InstalmentPlans
+            {
+                InstalmentPlan = new[]
+                {
+                    new InstalmentPlan { FinancialYear = "2023" }
+                }
+            }
+        };
+    }
+}
---
+++ TestApp/UsersDataModel.cs
@@ -0,0 +1,16 @@
+﻿namespace TestApp;
+
+public class UsersDataModel
+{
+    public InstalmentPlans InstalmentPlans { get; set; } = new();
+}
+
+public class InstalmentPlans
+{
+    public InstalmentPlan[] InstalmentPlan { get; set; } = [];
+}
+
+public class InstalmentPlan
+{
+    public string FinancialYear { get; set; } = string.Empty;
+}
